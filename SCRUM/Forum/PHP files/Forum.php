a1448b3a8310954845fa87a4925838c94f1f287a
timo <123752@student.drenthecollege.nl>
2019-05-07 09:39:28 +0200

    Forum
    Forum html/css

---
+++ SCRUM/Forum/PHP files/Forum.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Forum</title>
+  </head>
+  <body>
+
+<link rel="stylesheet" href="forum.css">
+
+<ul>
+  <li><a class="active" href="Forum.php">Homepage</a></li>
+  <li><a href="#news">Games</a></li>
+    <li><a href="#news">Programming</a></li>
+  <li><a href="Contact.php">Contact</a></li>
+  <li><a href="Registration.php">Account</a></li>
+</ul>
+<br>
+<br>
+<br>
+<link rel="stylesheet" href="forumtalk.css">
+<div class="container">
+  <img src="my logo.jpg" alt="Avatar">
+  <p>Where can I get info about games?</p>
+  <span class="time-right">11:00</span>
+</div>
+
+<div class="container darker">
+  <img src="sick logo.jpg" alt="Avatar">
+  <p>Go to the games page! You can get there with the NAV bar </p>
+  <span class="time-right">11:01</span>
+</div>
+
+<div class="container">
+  <img src="my logo.jpg" alt="Avatar">
+  <p>Thnx dude</p>
+  <span class="time-right">11:02</span>
+</div>
+
+<div class="container darker">
+  <img src="sick logo.jpg" alt="Avatar">
+  <p>No problem</p>
+  <span class="time-right">11:05</span>
+</div>
+
+
+
+<br><br><br>
+<br><br><br>
+<br><br><br>
+<br><br><br>
+<br><br><br>
+<br><br><br>
+
+
+<bottom> <h2>© Made by Timo</h2> </bottom>
+</html>
